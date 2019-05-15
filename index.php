7f21c465d1e868cd2e3a1ad7ddebc0722a24e42d
lilpkstud <lilpkstud@hotmail.com>
2019-05-15 11:38:21 -0700

    Created Index.php

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PMP Flash</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Index CSS -->
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>49 Processes and Process Groups</h1>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-light">
+                    <th scope="col"></th>
+                    <th scope="col">Init</th>
+                    <th scope="col">Plan</th>
+                    <th scope="col">Execute</th>
+                    <th scope="col">Monitor & Control</th>
+                    <th scope="col">Close</th>
+                </thead>
+                <tbody>
+                    <!-- Integration Management -->
+                    <tr>
+                        <th scope="col">
+                            <p>[4] Integration Management</p>
+                        </th>
+                        <td>
+                            <p>4.1: Develop Project Charter</p>
+                        </td>
+                        <td>
+                            <p>4.2: Plan Project Management</p>
+                        </td>
+                        <td>
+                            <p>4.3: Manage Project Work</p>
+                            <p>4.4: Manage Project Knowledge</p>
+                        <td>
+                            <p>4.5: Monitor and Control Project Work</p>
+                            <p>4.6: Perform Integrated Change Control</p>
+                        </td>
+                        <td>
+                            <p>4.7: Close Project/Phase</p>
+                        </td>
+                    </tr>
+                    <!-- Scope Management -->
+                    <tr class="table-warning">
+                        <th scope="col">
+                            <p>[5] Scope Management</p>
+                        </th>
+                        <td></td>
+                        <td>
+                            <p>5.1: Plan Scope Management</p>
+                            <p>5.2: Collect requirements</p>
+                            <p>5.3: Define Scope</p>
+                            <p>5.4: Create WBS</p>
+                        </td>
+                        <td></td>
+                        <td>
+                            <p>5.5: Validate Scope</p>
+                            <p>5.6: Control Scope</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <!-- Schedule Management -->
+                    <tr>
+                        <th scope="col">
+                            <p>[6] Schedule Management</p>
+                        </th>
+                        <td></td>
+                        <td>
+                            <p>6.1: Plan Schedule Management</p>
+                            <p>6.2: Define Activities</p>
+                            <p>6.3: Sequence Activities</p>
+                            <p>6.4: Estimate Activity Duration</p>
+                            <p>6.5: Create Schedule</p>
+                        </td>
+                        <td></td>
+                        <td>6.6: Control Schedule</td>
+                        <td></td>
+                    </tr>
+                    <!-- Cost Management -->
+                    <tr class="table-success">
+                        <th scope="col">
+                            <p>[7] Cost Management</p>
+                        </th>
+                        <td></td>
+                        <td>
+                            <p>7.1: Plan Cost Management</p>
+                            <p>7.2: Estimate Costs</p>
+                            <p>7.3: Determine Budget</p>
+                        </td>
+                        <td></td>
+                        <td>
+                            <p>7.4: Control Costs</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <!-- Quality Management -->
+                    <tr>
+                        <th scope="col">
+                            <p>[8] Quality Management</p>
+                        </th>
+                        <td></td>
+                        <td>8.1: Plan Quality Management</td>
+                        <td>8.2: Manage Quality</td>
+                        <td>8.3: Monitor Quality</td>
+                        <td></td>
+                    </tr>
+                    <!-- Resource Management -->
+                    <tr>
+                        <th scope="col">[9] Resource Management</th>
+                        <td></td>
+                        <td>
+                            <p>9.1: Plan Resource Management<p>
+                            <p>9.2: Estimate Activity Resources</p>
+                        </td>
+                        <td>
+                            <p>9.3: Acquire Resources</p>
+                            <p>9.4: Develop Team</p>
+                            <p>9.5: Manage Team</p>
+                        </td>
+                        <td>
+                            <p>9.6: Control Resources</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <!-- Communication Management -->
+                    <tr>
+                        <th scope="col">
+                            [10] Communication Management
+                        </th>
+                        <td></td>
+                        <td>10.1: Plan Communication Management</td>
+                        <td>10.2: Manage Communications</td>
+                        <td>10.3: Monitor Communications</td>
+                        <td></td>
+                    </tr>
+                    <!-- Risk Management -->
+                    <tr>
+                        <th scope="col">
+                            [11] Risk Management
+                        </th>
+                        <td></td>
+                        <td>
+                            <p>11.1: Plan Risk Management</p>
+                            <p>11.2: Identify Risks</p>
+                            <p>11.3: Perform Qualitative Risk Analysis</p>
+                            <p>11.4: Perform Quantitative Risk Analysis</p>
+                            <p>11.5: Plan Risk Responses</p>
+                        </td>
+                        <td>11.6: Implement Risk Responses</td>
+                        <td>11.7: Monitor Risks</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- [4] Integration Management -->
+    <div class="container">
+        <h3>[4] Integration Management</h3>
+        <div class="row">
+            <div class="col">4.1: Develop Project Charter</div>
+            <div class="col">definition</div>
+        </div>
+        <div class="row">
+            <div class="col">4.2: Plan Project Management</div>
+            <div class="col">definition</div>
+        </div>
+        <div class="row">
+            <div class="col">4.3: Manage Project Work</div>
+        </div>
+        <div class="row">
+            <div class="col">4.4: Manage Project Knowledge</div>
+        </div>
+        <div class="row">
+            <div class="col">4.5: Monitor and Control Project Work</div>
+        </div>
+        <div class="row">
+            <div class="col">4.6: Perform Integrated Change Control</div>
+        </div>
+        <div class="row">
+            <div class="col">4.7: Close Project/Phase</div>
+        </div>
+    </div>
+    <!-- [5] Scope Management -->
+    <div class="container">
+        <h3>[5] Scope Management </h3>
+        <div class="row">
+            <div class="col">5.1: Plan Scope Management</div>
+        </div>
+        <div class="row">
+            <div class="col">5.2: Collect Requirements</div>
+        </div>
+        <div class="row">
+            <div class="col">5.3: Define Scope</div>
+        </div>
+        <div class="row">
+            <div class="col">5.4: Create WBS</div>
+        </div>
+        <div class="row">
+            <div class="col">5.5: Validate Scope</div>
+        </div>
+        <div class="row">
+            <div class="col">5.6: Control Scope</div>
+        </div>
+    </div>
+    <!-- [6] Schedule Management -->
+    <div class="container">
+        <h3>[6]Schedule Management</h3>
+        <div class="row">
+            <div class="col">6.1: Plan Schedule Management</div>
+        </div>
+        <div class="row">
+            <div class="col">6.2: Define Activities</div>
+        </div>
+        <div class="row">
+            <div class="col">6.3: Sequence Activities</div>
+        </div>
+        <div class="row">
+            <div class="col">6.4: Estimate Activity Durations</div>
+        </div>
+        <div class="row">
+            <div class="col">6.5: Create Schedule</div>
+        </div>
+        <div class="row">
+            <div class="col">6.6: Control Schedule</div>
+        </div>
+    </div>
+    <!-- [7] Cost Management -->
+    <div class="container">
+        <h3>[7] Cost Management</h3>
+        <div class="row">
+            <div class="col">7.1: Plan Cost Management</div>
+        </div>
+        <div class="row">
+            <div class="col">7.2: Estimate Cost </div>
+        </div>
+        <div class="row">
+            <div class="col">7.3: Determine Budget</div>
+        </div>
+        <div class="row">
+            <div class="col">7.4: Control Costs</div>
+        </div>
+    </div>
+    <!-- [8] Quality Management -->
+    <div class="container">
+        <h4>[8] Quality Management</h4>
+        <div class="row">
+            <div class="col">8.1: Plan Quality Management</div>
+        </div>
+        <div class="row">
+            <div class="col">8.2: Manage Quality</div>
+        </div>
+        <div class="row">
+            <div class="col">8.3: Control Quality</div>
+        </div>
+    </div>
+    <!-- [9] Resources Management -->
+    <div class="container">
+        <h3>[9] Resource Management</h3>
+        <div class="row">
+            <div class="col">9.1: Plan Resource Management</div>
+        </div>
+        <div class="row">
+            <div class="col">9.2: Estimate Resource Duration</div>
+        </div><div class="row">
+            <div class="col">9.3: Acquire Resources</div>
+        </div><div class="row">
+            <div class="col">9.4: Develop Team</div>
+        </div><div class="row">
+            <div class="col">9.5: Manage Team</div>
+        </div>
+        <div class="row">
+            <div class="col">9.6: Monitor Resources</div>
+        </div>
+    </div>
+    <!-- [10] Communication Management -->
+    <div class="container">
+        <h3>[10] Communication Management</h3>
+        <div class="row">
+            <div class="col">10.1: Plan Communication Management</div>
+        </div>
+        <div class="row">
+            <div class="col">10.2: Manage Communications</div>
+        </div>
+        <div class="row">
+            <div class="col">10.3: Monitor Communications</div>
+        </div>
+    </div>
+    <!-- [11] Risk Management -->
+    <div class="container">
+        <div class="row">
+            <div class="col">10.1: Plan Quality Management</div>
+        </div>
+        <div class="row">
+            <div class="col">10.2: Identify Risks</div>
+        </div>
+        <div class="row">
+            <div class="col">10.3: Perform Qualitative Risk Analysis</div>
+        </div>
+        <div class="row">
+            <div class="col">10.4: Perform Quantitative Risk Analysis</div>
+        </div>
+        <div class="row">
+            <div class="col">10.5: Plan Risk Responses</div>
+        </div>
+        <div class="row">
+            <div class="col">10.6: Implement Risk Responses</div>
+        </div>
+        <div class="row">
+            <div class="col">10.7: Monitor Risks</div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
